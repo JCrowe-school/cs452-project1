e704eca19e238957189bdc92815c311df950ac8d
JCrowe-school <114427655+JCrowe-school@users.noreply.github.com>
2024-09-23 21:24:06 +0000

    task 3 complete

--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,17 @@
-/**Update this file with the starter code**/
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "lab.h"
+
+void parse_args(int argc, char **argv) {
+    int c;
+
+    while ((c = getopt (argc, argv, "v")) != -1) {
+        switch (c) {
+            case 'v':
+                printf("Version %d.%d", lab_VERSION_MAJOR, lab_VERSION_MINOR);
+                return 1;
+        }
+    }
+}
